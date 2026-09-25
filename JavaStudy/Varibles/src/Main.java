3b81be49f4dd4dfabaccc0110bc36f806c2ca77e
NorDajid <vandai06112007@gmail.com>
2026-09-25 21:20:56 +0700

    Update Main.java

--- JavaStudy/Varibles/src/Main.java
+++ JavaStudy/Varibles/src/Main.java
@@ -7,8 +7,8 @@ public class Main {
         // Ref: a varible point to an address, if 1 ref change then all the ref that have the same address changed too
         // Comparing example: Pri like bulbs that have independent flicks, while Ref have 1 switch connect to all.
         double GPA = 3.1;
-        double BonusGPA = 0.44;
-        GPA = GPA - Math.sin(GPA+BonusGPA-1);
+        double BonusGPA = 2.4;
+        GPA = GPA - Math.sin(GPA+BonusGPA-1.5);
         System.out.println(GPA);
     }
 }
